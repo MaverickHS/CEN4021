52ce7874024ed704bb525b7f1bf57a703437ffee
MaverickHS <maverickhasc@gmail.com>
2024-02-07 22:19:16 -0500

    Login Screen and Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <>
